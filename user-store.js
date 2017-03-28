2b8d2fb7d9f038b95341c48e99cb15634241b2d3
Krzysztof Zbiciński <fistasheq+github@gmail.com>
2017-03-28 21:15:28 +0200

    Make UserStore download real data

--- user-store.js
+++ user-store.js
@@ -1,19 +1,44 @@
-const DUMMY_USER_DATA = require('./dummy-data/user.json');
-const DUMMY_POSTS_DATA = require('./dummy-data/posts.json');
+const HttpClient = require('./http-client');
+const API_URL_BASE = 'http://jsonplaceholder.typicode.com';
 
 class UserStore {
-    getUser() {
-        return Promise.resolve(DUMMY_USER_DATA);
+
+    constructor() {
+        this.httpClient = new HttpClient();
     }
 
-    getPosts() {
-        return Promise.resolve(DUMMY_POSTS_DATA);
+    getUser(id) {
+        if(id) {
+            const url = `${API_URL_BASE}/users/${id}`;
+            return this.httpClient.get(url)
+                .then((rawData) => {
+                    const parsedData = JSON.parse(rawData);
+                    return Promise.resolve(parsedData);
+                });
+        }
+        else {
+            throw new Error('No user id provided.');
+        }
     }
 
-    getUserWithPosts() {
+    getPosts(userId) {
+        if(userId) {
+            const url = `${API_URL_BASE}/posts?userId=${userId}`;
+            return this.httpClient.get(url)
+                .then((rawData) => {
+                    const parsedData = JSON.parse(rawData);
+                    return Promise.resolve(parsedData);
+                });
+        }
+        else {
+            throw new Error('No user id provided.');
+        }
+    }
+
+    getUserWithPosts(userId) {
         return Promise.all([
-            this.getUser(),
-            this.getPosts()
+            this.getUser(userId),
+            this.getPosts(userId)
         ]).then((results) => {
             const user = results[0];
             const posts = results[1];
